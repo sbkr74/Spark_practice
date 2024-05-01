5895086c42cd6b44dee3ea668aa8e4338062c392
sbkr74 <sbkr74@gmail.com>
2024-05-01 18:06:30 +0530

    Approach 2 added using only spark dataframes

--- Q1_day1.py
+++ Q1_day1.py
@@ -2,12 +2,14 @@ import pandas as pd
 from pyspark.sql import SparkSession
 from datetime import datetime, timedelta
 import random
+import pyspark.sql.function as F
 
 # Create SparkSession
 spark = SparkSession.builder \
     .appName("Day1_practice") \
     .getOrCreate()
 
+# ==========================================================================================================================
 # Dummy data for instacart_reviews
 reviews_data = {
     "id": list(range(1, 501)),
@@ -45,10 +47,11 @@ stores_df_pandas = pd.DataFrame(stores_data)
 
 # Convert to Spark DataFrame
 stores_df_spark = spark.createDataFrame(stores_df_pandas)
-
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Create temporary views for Spark DataFrames
 reviews_df_spark.createOrReplaceTempView("instacart_reviews")
 stores_df_spark.createOrReplaceTempView("instacart_stores")
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 query = """with CTE as (
             select s.name,r.score,case when r.score>5 then 'positive' else 'negative' end as review from instacart_stores s join instacart_reviews r on s.id = r.store_id 
@@ -64,3 +67,31 @@ result = spark.sql(query)
 print("Records in result:")
 result.show(result.count())
 print("Values in result:",result.count())
+# ==========================================================================================================================
+# Approach 2:
+# step 1: joined two dfs(inner)
+joined_df = reviews_df_spark.join(stores_df_spark,reviews_df_spark.store_id == stores_df_spark.id,"inner")
+
+#stpe 2: filtered df(year = `2021`,month = 2nd half of the year)
+filtered_df = joined_df.filter((F.year('opening_date')==F.lit('2021')) & (F.month('opening_date')>=F.lit('6')))
+
+#step 3: added a column `review`(if rating is below 5 then `Negative` else `Positive`)
+review_df = filtered_df.withColumn('review',F.when((filtered_df.score>5),F.lit("Positive")).otherwise(F.lit("Negative")))
+
+# step 4: grouped and aggregiated (getting sum of positive and negative and total sum of both when group together based on `name`)
+review_rate_df= review_df.withColumn('pos',F.when((review_df.review == 'Positive'), F.lit(1)).otherwise(F.lit(0)))\
+    .withColumn('neg',F.when((review_df.review == 'Negative'), F.lit(1)).otherwise(F.lit(0)))\
+    .groupby('name')\
+    .agg(F.sum('pos').alias('pos_sum'),F.sum('neg').alias('neg_sum'))\
+    .withColumn('group_count',F.expr("pos_sum+neg_sum"))
+
+# getting ratio((positive/total) and (negative/total))
+review_ratio_df = review_rate_df.withColumn('pos_ratio',F.round(F.col('pos_sum')*100/F.col('group_count'),2))\
+                    .withColumn('neg_ratio',F.round(F.col('neg_sum')*100/F.col('group_count'),2))
+
+# filtering(show results only greater than 20% of negative ratio)
+review_ratio_df = review_ratio_df.filter(F.col('neg_ratio')>20).select('name','pos_ratio','neg_ratio')
+
+# Display the final output
+review_ratio_df.show(review_ratio_df.count())
+print(review_ratio_df.count())
