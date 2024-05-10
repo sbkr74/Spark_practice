bc12d14dac7cfcd8c5956435a8b68504a0340793
sbkr74 <sbkr74@gmail.com>
2024-05-10 21:39:49 +0530

    update2.1 file Q3_day3.py with SQL approach

--- Q3_day3.py
+++ Q3_day3.py
@@ -25,3 +25,24 @@ def generate_data(num_records):
 # Generate sample data
 sample_data = generate_data(500)
 pandas_df = pd.DataFrame(sample_data)
+####################################################################################################################
+####################################################################################################################
+# Spark SQL approach
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("day3").getOrCreate()
+spark_df = spark.createDataFrame(pandas_df)
+# spark_df.show(spark_df.count())
+
+spark_df.createOrReplaceTempView("revenue")
+df = spark.sql("select *, date_part('month',created_at) as month from revenue order by created_at,month")
+df.show(df.count())
+print(df.count())
+
+df1 = spark.sql("select sum(value) as total, date_part('month',created_at) as month from revenue group by month order by month")
+df1.show(df1.count())
+print(df1.count())
+
+df1.createOrReplaceTempView("monthly_revenue")
+df2 = spark.sql("select t1.*,round((((t1.total-t2.total)/t2.total)*100),2) as diff_per from monthly_revenue t1 left join monthly_revenue t2 on t1.month=t2.month+1 order by month")
+df2.show()
