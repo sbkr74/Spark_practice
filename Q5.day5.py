4e38bc0814a99a40df9214f6d42b114c7ff7c270
sbkr74 <sbkr74@gmail.com>
2024-06-08 23:28:52 +0530

    Q5_day5.py file updated(sql approach)

--- Q5.day5.py
+++ Q5.day5.py
@@ -6,11 +6,30 @@ student = [(1,'Steve'),(2,'David'),(3,'Aryan')]
 student_schema = "student_id int , student_name string"
 
 student_df = spark.createDataFrame(data = student,schema = student_schema)
-student_df.show()
+# student_df.show()
 
 marks = [(1,'pyspark',90), (1,'sql',100), (2,'sql',70), (2,'pyspark',60), (3,'sql',30), (3,'pyspark',20) ] 
 marks_schema = "student_id int , subject_name string , marks int"
 
 marks_df = spark.createDataFrame(data = marks, schema = marks_schema)
-marks_df.show()
+# marks_df.show()
 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# SQL approach
+student_df.createOrReplaceTempView('students')
+marks_df.createOrReplaceTempView('marks')
+
+
+# 1. % Marks greater than or equal to 70 then 'Distinction'
+query = ''' select s.student_id,student_name,marks,
+(case when marks>= 70 then 'Distinction'
+    when marks<=69 and marks>=60 then 'First class'
+    when marks<=59 and marks>=50 then 'Second class'
+    when marks<=49 and marks>=40 then 'Third class'
+ else 'Fail' end) as grade
+from students s inner join marks m 
+on s.student_id = m.student_id 
+'''
+sql_df = spark.sql(query)
+sql_df.show()
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
