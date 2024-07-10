f7064bcf65c455e172b40ee8654dcc375f358875
sbkr74 <sbkr74@gmail.com>
2024-07-10 20:45:45 +0530

    Q9_day9.py file updated2.0(spark approach)

--- Q9_day9.py
+++ Q9_day9.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.window import Window
 
 # Create a SparkSession with the desired GC configurations
 spark = SparkSession.builder \
@@ -18,4 +20,11 @@ df.show()
 df.createOrReplaceTempView('students')
 query = 'select id,case when id = 1 then 2 when id = 3 then 4  else id end as new_id,student  from students where id in(1,2,3,4,5)'
 sql_df = spark.sql(query)
-sql_df.show()
+# sql_df.show()
+
+#########################################################
+# Spark DataFrame Approach
+df_lead = df.withColumn('next_val',lead(col('student')).over(Window.orderBy(col('id'))))
+df_lag = df_lead.withColumn('prev_val',lag(col('student')).over(Window.orderBy(col('id'))))
+spark_df = df_lag.withColumn('Exchange_seat',when(col('id')%2==1,coalesce(col('next_val'),col('student'))).otherwise(col('prev_val')))
+spark_df.select("id","student","Exchange_seat").show()
