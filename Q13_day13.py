c879d63e5a6274491fab78d16144155f1e49392d
sbkr74 <sbkr74@gmail.com>
2024-07-12 21:48:08 +0530

    Q13_day13.py file updated1.0(casting)

--- Q13_day13.py
+++ Q13_day13.py
@@ -8,13 +8,19 @@ sampleData=[(1,"A",20,"31|32|34"),
  (4,"D",23,"10|12|12")]
 
 # SparkSession
-spark = SparkSession.builder.appName('Day_13').getOrCreate()
+spark = SparkSession.builder.appName('Day_13')\
+        .config("spark.eventLog.gcMetrics.youngGenerationGarbageCollectors","G1 Young Generation, G1 Concurrent GC")\
+        .config("spark.eventLog.gcMetrics.oldGenerationGarbageCollectors","G1 Old Generation, G1 Concurrent GC")\
+        .getOrCreate()
+
+# Dataframe
 df = spark.createDataFrame(sampleData).toDF("ID","NAME","AGE","MARKS")
-df.show()
-df1 = df.withColumn("MARKS",split("MARKS","\\|"))
+df1 = df.withColumn("MARKS",split("MARKS","\\|")\
+        .cast("array<int>")\
+        .alias("marks"))\
+        .withColumn("Physics",col("marks").getItem(0).cast("int"))\
+        .withColumn("Chemistry",col("marks").getItem(1).cast("int"))\
+        .withColumn("Maths",col("marks").getItem(2).cast("int"))\
+        .drop("MARKS")
 df1.show()
-df2 = df1.withColumn("Physics",col('MARKS').getItem(0))\
-         .withColumn("Chemistry",col("MARKS").getItem(1))\
-         .withColumn("Maths",col("MARKS").getItem(2))
-df2 = df2.drop("MARKS")
-df2.show()
+        
