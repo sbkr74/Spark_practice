83ed2e07c7110cad25a623bc07bc9b103533c3c7
sbkr74 <sbkr74@gmail.com>
2024-07-13 07:49:27 +0530

    Q14_day14.py file added

---
+++ Q14_day14.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.appName('Day_14')\
+        .config("spark.eventLog.gcMetrics.youngGenerationGarbageCollectors","G1 Young Generation, G1 Concurrent GC")\
+        .config("spark.eventLog.gcMetrics.oldGenerationGarbageCollectors","G1 Old Generation, G1 Concurrent GC")\
+        .getOrCreate()
+
+data = [
+ (1, "Gaurav",["Pune", "Bangalore", "Hyderabad"]),
+ (2, "Rishabh",["Mumbai", "Bangalore", "Pune"])
+]
+
+df = spark.createDataFrame(data).toDF("EmpId","Name","Locations")
+df1 = df.withColumn("Locations",explode(col("Locations")))
+df1.show()
+
