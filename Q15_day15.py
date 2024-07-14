8fe727b5a1e1e2ce3ee76d7775d0eb140b8ef85b
sbkr74 <sbkr74@gmail.com>
2024-07-15 00:13:57 +0530

    Q15_Day15.py file updated1.3(used struct schema for spark df )

--- Q15_day15.py
+++ Q15_day15.py
@@ -1,30 +1,53 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from datetime import datetime
 
 spark = SparkSession.builder.appName('Day_15')\
         .config("spark.eventLog.gcMetrics.youngGenerationGarbageCollectors","G1 Young Generation, G1 Concurrent GC")\
         .config("spark.eventLog.gcMetrics.oldGenerationGarbageCollectors","G1 Old Generation, G1 Concurrent GC")\
         .getOrCreate()
 
+# data = [
+#     ('A','2023-01-15','100.0'),
+#     ('B','2023-01-20','150.0'),
+#     ('A','2023-02-10','120.0'),
+#     ('B','2023-02-15','180.0'),
+#     ('C','2023-03-05','200.0'),
+#     ('A','2023-03-10','250.0')
+# ]
+
+#########################################################################################
 data = [
-    ('A','2023-01-15','100.0'),
-    ('B','2023-01-20','150.0'),
-    ('A','2023-02-10','120.0'),
-    ('B','2023-02-15','180.0'),
-    ('C','2023-03-05','200.0'),
-    ('A','2023-03-10','250.0')
+    ('A',datetime.strptime("2023-01-15", "%Y-%m-%d").date(),100.0),
+    ('B',datetime.strptime("2023-01-20", "%Y-%m-%d").date(),150.0),
+    ('A',datetime.strptime("2023-02-10", "%Y-%m-%d").date(),120.0),
+    ('B',datetime.strptime("2023-02-15", "%Y-%m-%d").date(),180.0),
+    ('C',datetime.strptime("2023-03-05", "%Y-%m-%d").date(),200.0),
+    ('A',datetime.strptime("2023-03-10", "%Y-%m-%d").date(),250.0)
 ]
+schema = StructType([
+    StructField("product_id", StringType(), True),
+    StructField("sales_date", DateType(), True),
+    StructField("sales_amount", DoubleType(), True)
+    
+])
+#########################################################################################
 
-df = spark.createDataFrame(data).toDF('product_id','sales_date','sales_amount')
+# df = spark.createDataFrame(data).toDF('product_id','sales_date','sales_amount')
 
+df = spark.createDataFrame(data,schema)
+# df.printSchema()
+df = df.withColumn("month",date_format('sales_date','yyyy-MM'))
 
-df = df.withColumn("sales_date",to_date(df['sales_date'],'yyyy-MM-dd'))\
-    .withColumn("sales_amount",col('sales_amount').cast("float"))\
-    .withColumn("month",date_format('sales_date',"yyyy-MM"))
+# df = df.withColumn("sales_date",to_date(df['sales_date'],'yyyy-MM-dd'))\
+#     .withColumn("sales_amount",col('sales_amount').cast("float"))\
+#     .withColumn("month",date_format('sales_date',"yyyy-MM"))
 
 # df1 = df.withColumn('year',year(df['sales_date']))\
 #         .withColumn('month',month(df['sales_date']))
 # df1.show()
+
 df2 = df.withColumn('month',date_format('sales_date',"yyyy-MM"))\
         .groupBy('month').agg(max(df.sales_amount))
 df3 = df.join(df2,(df['month'] == df2['month'])  & (df["sales_amount"] == df2["max(sales_amount)"]),"inner")\
