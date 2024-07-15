7458cb00367d2347f40cbdf7e6136fa719c56dfb
sbkr74 <sbkr74@gmail.com>
2024-07-15 21:31:44 +0530

    Q16_day16.py file updated1.0(cleaning,renaming,casting)

--- Q16_day16.py
+++ Q16_day16.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-
+from pyspark.sql.functions import *
 spark = SparkSession.builder.appName('Day_16')\
         .config("spark.eventLog.gcMetrics.youngGenerationGarbageCollectors","G1 Young Generation, G1 Concurrent GC")\
         .config("spark.eventLog.gcMetrics.oldGenerationGarbageCollectors","G1 Old Generation, G1 Concurrent GC")\
@@ -14,4 +14,17 @@ data = [
 ]
 
 df = spark.createDataFrame(data)
+
+# cleaning
+df = df.withColumn("phone",regexp_replace("phone","[^0-9]",""))
+
+# fill null values
+df = df.fillna({"phone":"N/A"})
+
+# renaming column
+df = df.withColumnRenamed("name","full_name")
+
+# changing datatype of column
+df1 = df.withColumn("registration_date",to_date(df['registration_date'],"yyyy-MM-dd"))
+
 df.show()
