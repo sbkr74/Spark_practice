afc52e6ae8dd10f09317b7e3a0532d47a3e1b1a6
sbkr74 <sbkr74@gmail.com>
2024-07-19 22:01:31 +0530

    Q18_day18.py file updated2.1(minor)

--- Q18_day18.py
+++ Q18_day18.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
 
-spark = SparkSession.builder.appName('Day_17')\
+spark = SparkSession.builder.appName('Day_18')\
         .config("spark.eventLog.gcMetrics.youngGenerationGarbageCollectors","G1 Young Generation, G1 Concurrent GC")\
         .config("spark.eventLog.gcMetrics.oldGenerationGarbageCollectors","G1 Old Generation, G1 Concurrent GC")\
         .getOrCreate()
