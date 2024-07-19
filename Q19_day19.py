dd1935e06b949476c90610b65918db84d22254f0
sbkr74 <sbkr74@gmail.com>
2024-07-19 20:36:08 +0530

    Q19_day19.py file updated1.0(used groupby)

--- Q19_day19.py
+++ Q19_day19.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import *
 
 
 spark = SparkSession.builder.appName('Day_17')\
@@ -7,13 +7,16 @@ spark = SparkSession.builder.appName('Day_17')\
         .config("spark.eventLog.gcMetrics.oldGenerationGarbageCollectors","G1 Old Generation, G1 Concurrent GC")\
         .getOrCreate()
 
+spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
 data = [( 
 '3000' , '22-may'), 
 ('5000' , '23-may'),
-('5000' , '25-may'),
+('6000' , '25-may'),
 ('10000' , '22-june'), 
 ('1250' , '03-july')]
 schema = ['revenue','date']
 
 df = spark.createDataFrame(data,schema)
-df.show()
+df1 = df.withColumn("month",date_format(to_date(col("date"),'dd-MMM'),'MMM'))
+df1.groupby('month').agg(sum(col('revenue')).alias("total_revenue")).show()
+
