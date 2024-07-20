ad66ec272be993320fabff63a4fe5fdb93be1fb2
sbkr74 <sbkr74@gmail.com>
2024-07-20 23:45:47 +0530

    spark_stream_test.py file updated1.0(checking approaches)

--- spark_stream_test.py
+++ spark_stream_test.py
@@ -1,13 +1,58 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.types import *
+# from pyspark.sql import SparkSession
+# from pyspark.sql.types import *
 
+# spark = SparkSession.builder \
+#     .appName("FileStream") \
+#     .config("spark.hadoop.io.native.lib.available", "false") \
+#     .config("spark.sql.streaming.forceDeleteTempCheckpointLocation", "true") \
+#     .getOrCreate()
+
+# input_path = r"D:\Learning\Spark_practice\data\test\sample1.json"
+
+# # Define the schema for the JSON files
+# schema = StructType([
+#     StructField("fruit", StringType(), True),
+#     StructField("size", StringType(), True),
+#     StructField("color", StringType(), True),
+#     StructField("quantity", IntegerType(), True)
+#     # Add more fields as necessary
+# ])
+
+# # Read the JSON files as a streaming DataFrame
+# df = spark.readStream \
+#     .schema(schema) \
+#     .format("json") \
+#     .option("maxFilesPerTrigger", 1) \
+#     .load(input_path)
+
+# # Example processing: Select specific columns
+# processed_df = df.select("quantity", "size")
+
+# query = processed_df.writeStream \
+#     .outputMode("append") \
+#     .format("console") \
+#     .start()
+
+# query.awaitTermination()
+
+######################################################################################
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+# Set Hadoop home and add winutils to the PATH
+os.environ['HADOOP_HOME'] = "C:\\hadoop"
+os.environ['PATH'] += os.pathsep + os.path.join(os.environ['HADOOP_HOME'], 'bin')
+
+# Create Spark session with necessary configurations
 spark = SparkSession.builder \
     .appName("FileStream") \
-    .config("spark.hadoop.io.native.lib.available", "false") \
     .config("spark.sql.streaming.forceDeleteTempCheckpointLocation", "true") \
+    .config("spark.hadoop.io.native.lib.available", "false") \
     .getOrCreate()
 
 input_path = r"D:\Learning\Spark_practice\data\test\sample1.json"
+checkpoint_path = r"D:\Learning\Spark_practice\checkpoint"
 
 # Define the schema for the JSON files
 schema = StructType([
@@ -15,7 +60,6 @@ schema = StructType([
     StructField("size", StringType(), True),
     StructField("color", StringType(), True),
     StructField("quantity", IntegerType(), True)
-    # Add more fields as necessary
 ])
 
 # Read the JSON files as a streaming DataFrame
@@ -31,7 +75,7 @@ processed_df = df.select("quantity", "size")
 query = processed_df.writeStream \
     .outputMode("append") \
     .format("console") \
+    .option("checkpointLocation", checkpoint_path) \
     .start()
 
 query.awaitTermination()
-
