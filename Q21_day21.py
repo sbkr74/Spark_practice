2842e07f2cdc0fbbd44e86e9f96cc26fa352f249
sbkr74 <sbkr74@gmail.com>
2025-04-11 23:02:07 +0530

    update1.0 [joined two dataframes]

--- Q21_day21.py
+++ Q21_day21.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import rank
+from pyspark.sql.functions import col,rank,min,max
 from pyspark.sql.window import Window
 
 spark = SparkSession.builder.appName("Day 21").getOrCreate()
@@ -18,5 +18,8 @@ data = [(1, 'Flight2', 'Goa', 'Kochi'),
 
 df = spark.createDataFrame(data).toDF("cust_id","flight_id","origin","destination")
 
-df1 = df.withColumn("rank",rank().over(Window.partitionBy("cust_id").orderBy("flight_id")))
-df1.show()
+df1 = df.withColumn("order",rank().over(Window.partitionBy(col("cust_id")).orderBy(col("flight_id"))))
+df2 = df1.groupBy(col("cust_id")).agg(min(col("order")).alias('start'),max(col("order")).alias('end'))
+
+df_join = df2.join(df1,on=(df1.cust_id==df2.cust_id))
+df_join.show()
