3d8a042919a26123c61ee9352a55db52a3d4a159
sbkr74 <sbkr74@gmail.com>
2025-04-17 18:25:41 +0530

    update 2.0 [class & object](performance degradation)

--- Q22_day22.py
+++ Q22_day22.py
@@ -1,29 +1,42 @@
+import os, sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import row_number,col,desc,max,min
+from pyspark.sql.functions import col, row_number, desc
 from pyspark.sql.window import Window
 
-spark = SparkSession.builder.appName('Day 22').getOrCreate()
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+spark = SparkSession.builder.master('local[*]').getOrCreate()
 
-data = [
- ('A1', 'ADULT', 54),
- ('A2', 'ADULT', 53),
- ('A3', 'ADULT', 52),
- ('A4', 'ADULT', 58),
- ('A5', 'ADULT', 54),
- ('C1', 'CHILD', 20),
- ('C2', 'CHILD', 19),
- ('C3', 'CHILD', 22),
- ('C4', 'CHILD', 15)
- ]
+class AdultChildPair:
+ def create_data(self):
+    data = [
+    ('A1', 'ADULT', 54),
+    ('A2', 'ADULT', 53),
+    ('A3', 'ADULT', 52),
+    ('A4', 'ADULT', 58),
+    ('A5', 'ADULT', 54),
+    ('C1', 'CHILD', 20),
+    ('C2', 'CHILD', 19),
+    ('C3', 'CHILD', 22),
+    ('C4', 'CHILD', 15)
+    ]
+    columns = ['person', 'type', 'age']
+    return spark.createDataFrame(data, columns)
 
-df = spark.createDataFrame(data).toDF("person","type","age")
+ def get_pair(self, input_df):
+    adult_df = input_df.filter(col('type').__eq__('ADULT')) \
+    .withColumn('rnk', row_number().over(Window.orderBy(desc('age')).orderBy(desc('person'))))
 
-# Adult Dataframe
-df_adult = df.filter(col("type") == "ADULT").withColumn("rnk",row_number().over(Window.partitionBy(col("type")).orderBy(desc("age"))))
+    child_df = input_df.filter(col('type').__eq__('CHILD'))\
+    .withColumn('rnk', row_number().over(Window.orderBy('age').orderBy('person')))
 
-# Child Dataframe
-df_child = df.filter(col("type") == "CHILD").withColumn("rnk",row_number().over(Window.partitionBy(col("type")).orderBy(col("age"))))
+    result_df = adult_df.alias('A').join(child_df.alias('C'), on='rnk', how='full')\
+    .select(col('A.person'),
+    col('C.person'))
+    return result_df
 
-# joined using full so nothing will miss out from both side
-result_df = df_adult.alias("A").join(df_child.alias("C"),on=(df_adult.rnk==df_child.rnk),how="full").select(col('A.person').alias("ADULT"),col('C.person').alias("CHILD"))
+ob = AdultChildPair()
+input_df = ob.create_data()
+
+result_df = ob.get_pair(input_df)
 result_df.show()
